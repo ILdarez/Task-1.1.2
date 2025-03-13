646f1af79caa13e23d32d049d69faf8545ac77d1
ILdarez <ildareyy@gmail.com>
2025-03-13 21:22:51 +0500

    initial commit

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,25 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String DB_DRIVER ="com.mysql.jdbc.Driver";
+    private static final String DB_URL ="jdbc:mysql://localhost:3306/project_test";
+    private static final String DB_USERNAME ="ILdar";
+    private static final String DB_PASSWORD ="root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("Connected to database");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Failed to connect to database");
+        }
+        return connection;
+    }
+}
